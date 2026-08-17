b7ec7cacd301af78359239b5cbfd5831b3a7d83b
ChrisArce88 <chris.arce882@gmail.com>
2026-08-17 11:52:39 -0700

    Add Google Tag Manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import TermlyCMP from "./components/TermlyCMP";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,13 +24,17 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
 
       
-      <body suppressHydrationWarning>{children}
+      <body suppressHydrationWarning>
 
-        <TermlyCMP 
+          <TermlyCMP 
           websiteUUID="11c18093-6df0-48c0-b10c-5e43c9134c49" 
           autoBlock={true} 
           masterConsentsOrigin="" 
         />
+        
+         <GoogleTagManager gtmId="GTM-KHBM2GZ4" />
+
+        {children}
 
 
       </body>
