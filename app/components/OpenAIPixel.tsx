48e3992d1e60722e82677fcf691a2914521b627f
ChrisArce88 <chris.arce882@gmail.com>
2026-09-25 09:50:32 -0700

    fix: add OpenAI Ads pixel component

---
+++ app/components/OpenAIPixel.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { useEffect } from "react";
+
+export default function OpenAIPixel() {
+  useEffect(() => {
+    if (window.oaiq) return;
+
+    const q = (...args: any[]) => {
+      q.q.push(args);
+    };
+
+    q.q = [];
+
+    window.oaiq = q;
+
+    const script = document.createElement("script");
+    script.async = true;
+    script.src = "https://bzrcdn.openai.com/sdk/oaiq.min.js";
+
+    const firstScript = document.getElementsByTagName("script")[0];
+    firstScript.parentNode?.insertBefore(script, firstScript);
+    
+    window.oaiq("init", {
+      pixelId: "REcoysE6Po2fPcjdsTvWJp",
+      debug: true,
+    });
+  }, []);
+
+  return null;
+}
